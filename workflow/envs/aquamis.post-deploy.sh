1ab81febfe0c6bbcf6e6f66475771423a4add390
gregdenay <gregoire.denay@cvua-rrw.de>
2023-09-26 13:50:39 +0200

    updated aquamis to 1.4.0

--- workflow/envs/aquamis.post-deploy.sh
+++ workflow/envs/aquamis.post-deploy.sh
@@ -1,103 +1,135 @@
 #!/usr/bin/env bash
-set -Eeu
 
-# Repo URL
-aquamis_repo="https://gitlab.com/bfr_bioinformatics/AQUAMIS.git"
+# patch setup script with github version (conda version missing --env_name argument)
 
-# Commit hash to use
-commit="0290eb2acafd697f631b8b3a6135d2710519b575"
-
-# Local directory to save the Repo
-local_dir="$HOME/.nrw-geuebt/aquamis"
-
-# if already exists, wipe it clean and redo clone
-[ -d "$local_dir" ] && rm -rf "$local_dir"
-
-# clone and checkout repo
-echo "Cloning AQUAMIS and checking out stable commit"
-git clone -q "$aquamis_repo" "$local_dir"
-cd "$local_dir"
-git checkout "$commit"
-
-# Patch wrapper and Snakefile
-# echo "Applying patches"
-# patch -s --directory="$local_dir" --strip=1 << END
-# diff --unified --recursive --no-dereference chewieSnake-orig/chewieSnake_join.py chewieSnake/chewieSnake_join.py
-# --- chewieSnake-orig/chewieSnake_join.py	2023-05-30 10:12:16.855136923 +0200
-# +++ chewieSnake/chewieSnake_join.py	2023-05-30 10:14:42.000000000 +0200
-# @@ -147,7 +147,7 @@
-     # parser.add_argument('--clustering_method', help = 'The agglomeration method to be used for hierarchical clustering. This should be (an unambiguous abbreviation of) one of "ward.D", "ward.D2", "single", "complete", "average" (= UPGMA), "mcquitty" (= WPGMA), "median" (= WPGMC) or "centroid" (= UPGMC); default: single',
-                         # default = "single", required = False)
-     # parser.add_argument('--distance_threshold', help = 'A single distance threshold for the exctraction of sub-trees; default: 10',
-# -                        default = 10, required = False)
-# +                        default = 10, type=int, required = False)
-     # parser.add_argument('--cluster_names', help = 'A file with potential names for cluster names, one name per line, default: repo_path/scripts/cluster_names_reservoir.txt',
-                         # default = os.path.join(repo_path, "scripts", "cluster_names_reservoir.txt"), required = False)
-     # parser.add_argument('--subcluster_thresholds', help = 'A list of distance thresholds for subclustering; default: [3]',
-# diff --unified --recursive --no-dereference chewieSnake-orig/chewieSnake_join.smk chewieSnake/chewieSnake_join.smk
-# --- chewieSnake-orig/chewieSnake_join.smk	2023-05-30 10:12:16.451155752 +0200
-# +++ chewieSnake/chewieSnake_join.smk	2023-05-30 10:14:52.000000000 +0200
-# @@ -76,7 +76,7 @@
-         # "merged_db/sample_cluster_information.tsv", # from rule clustering
-         # "merged_db/samples2clusters.csv" # result from checkpoint
-         # "merged_db/listofreports.txt", # result from checkpoint
-# -        "merged_db/report.html"
-# +        # "merged_db/report.html"
+patch $CONDA_PREFIX/opt/aquamis/scripts/aquamis_setup.sh <<'EOF'
+--- aquamis_setup-old.sh	2023-09-19 21:57:10.000000000 +0200
++++ aquamis_setup-new.sh	2023-09-26 09:30:14.000000000 +0200
+@@ -30,6 +30,7 @@
+ For more information, please visit https://gitlab.com/bfr_bioinformatics/AQUAMIS
  
- # rule all_reportonly:
-     # input:
+ ${bold}Options:${normal}
++  -e, --env_name             Override conda environment name derived from envs/aquamis.yaml (default: aquamis)
+   -m, --mamba                Install the latest version of 'mamba' to the Conda base environment and
+                              create the AQUAMIS environment from the git repository recipe
+   -b, --busco                Download Augustus and BUSCO databases to <AQUAMIS>/download and extract them in the conda environment
+@@ -38,6 +39,7 @@
+   -s, --status               Show installation status
+   -f, --force                Force overwrite for downloads in <AQUAMIS>/download
+   -k, --keep_downloads       Do not remove downloads after extraction
++  -a, --auto                 Do not ask for interactive confirmation
+   -v, --verbose              Print script debug info
+   -h, --help                 Show this help
+ 
+@@ -54,7 +56,7 @@
+   local args=("$@")
+ 
+   ## default values of variables
+-  conda_recipe="aquamis.yaml"
++  conda_recipe="$repo_path/envs/aquamis.yaml"
+ 
+   ## default values of switches
+   conda_status=false
+@@ -68,6 +70,7 @@
+   arg_status=false
+   arg_force=false
+   arg_keep_dl=false
++  interactive=true
+ 
+   while :; do
+     case "${1-}" in
+@@ -76,6 +79,10 @@
+         set -x
+         shift
+         ;;
++      -e | --env_name)
++        conda_recipe_env_name="$2"
++        shift 2
++        ;;
+       -m | --mamba)
+         arg_mamba=true
+         shift
+@@ -104,6 +111,10 @@
+         arg_keep_dl=true
+         shift
+         ;;
++      -a | --auto)
++        interactive=false
++        shift
++        ;;
+       -?*) die "Unknown option: $1. For the manual, type ${blue}bash $script_real --help${normal}" ;;
+       *) break ;;
+     esac
+@@ -114,6 +125,7 @@
+ 
+   ## checks
+   [[ $(basename $script_path) != "scripts" ]] && die "This setup script does not reside in its original installation directory. This is a requirement for proper execution. Aborting..."
++  [[ -f $conda_recipe ]] || die "The conda recipe does not exist: $conda_recipe"
+ 
+   return 0
+ }
+@@ -128,7 +140,10 @@
+ show_info() {
+   logheader "Parameters:"
+   echo "
+-repo_path:|$repo_path
++Repository:|$repo_path
++Conda Recipe:|$conda_recipe
++Conda Recipe Env Name:|$conda_recipe_env_name
++Keep Downloads:|$arg_keep_dl
+ " | column -t -s "|"
+ }
+ 
+@@ -137,8 +152,8 @@
+ download_file() {
+   # Init
+   download_success=false
+-  download_status=false
+-  download_hash=''
++  local download_status=false
++  local download_hash=''
+   
+   # Download
+   if [[ -f $local_archive ]] && [[ $arg_force == false ]]; then
+@@ -167,10 +182,10 @@
+   [[ "$mamba_status" == true ]] && logwarn "Skipping Mamba installation. Mamba is already detected."
+ 
+   # Create Conda Environment for AQUAMIS
+-  logentry "Creating the AQUAMIS environment with the Conda recipe: $repo_path/envs/${conda_recipe}"
++  logentry "Creating the AQUAMIS environment with the Conda recipe: $conda_recipe"
+   set +eu  # workaround: see https://github.com/conda/conda/issues/8186
+-  [[ -z "${conda_env_path}" ]] && { mamba env create -f $repo_path/envs/${conda_recipe} || true; }
+-  [[ -n "${conda_env_path}" && "$arg_force" == true && ! -d "${conda_base}/envs/${conda_env_name}" ]] && { mamba env create -f $repo_path/envs/${conda_recipe} -p ${conda_base}/envs/${conda_env_name} || true; }  # corner case: there is already a conda env with the same name in another conda_base, e.g. NGSAdmin
++  [[ -z "${conda_env_path}" ]] && { mamba env create -n ${conda_recipe_env_name} -f $conda_recipe || true; }
++  [[ -n "${conda_env_path}" && "$arg_force" == true && ! -d "${conda_base}/envs/${conda_env_name}" ]] && { mamba env create -f $conda_recipe -p ${conda_base}/envs/${conda_env_name} || true; }  # corner case: there is already a conda env with the same name in another conda_base, e.g. NGSAdmin
+   [[ -n "${conda_env_path}" ]] && logwarn "A Conda environment with the name \"$conda_env_name\" is already present. Skipping environment creation."
+   # conda activate $conda_env_name
+   set -eu
+@@ -271,7 +286,7 @@
+   [[ -f $repo_path/reference_db/kraken/hash.k2d ]] && status_kraken="${green}OK${normal}"
+   [[ -f $repo_path/reference_db/mash/mashDB.msh ]] && status_mash="${green}OK${normal}"
+   [[ -f $repo_path/reference_db/taxonkit/names.dmp ]] && status_taxonkit="${green}OK${normal}"
+-  [[ -f ${conda_env_path:-$CONDA_PREFIX}/lib/python3.7/site-packages/quast_libs/busco/busco.py ]] && status_busco="${green}OK${normal}"
++  [[ -d ${conda_env_path:-$CONDA_PREFIX}/lib/python3.7/site-packages/quast_libs/busco/bacteria ]] && status_busco="${green}OK${normal}"
+   [[ -f ${conda_env_path:-$CONDA_PREFIX}/lib/python3.7/site-packages/quast_libs/augustus3.2.3/bin/augustus ]] && status_augustus="${green}OK${normal}"
+   logheader "Database Status:"
+   echo "
+@@ -291,8 +306,10 @@
+ logentry "Arguments: ${*:-"No arguments provided"}"
+ 
+ ## Workflow
+-conda_recipe_env_name=$(head -n1 $repo_path/envs/${conda_recipe} | cut -d' ' -f2)
++conda_recipe_env_name=${conda_recipe_env_name:-$(head -n1 $conda_recipe | cut -d' ' -f2)}
++show_info
+ check_conda $conda_recipe_env_name
++[[ "$interactive" == true ]] && interactive_query
+ [[ "$arg_mamba" == true ]] && setup_mamba
+ [[ "$arg_busco" == true ]] && complete_busco
+ [[ "$arg_databases" == true ]] && download_databases
 
-# END
+EOF
 
-# update env with aquamis.yaml
-if [[ $(which mamba) == "" ]]; then
-  conda env update --file "$local_dir/envs/auamis.yaml" -p $CONDA_PREFIX &> "$local_dir/deploy_log.txt"
-  # conda update -y -p $CONDA_PREFIX "python>=3.9" &>> "$local_dir/deploy_log.txt"
-else
-  mamba env update --file "$local_dir/envs/aquamis.yaml" -p $CONDA_PREFIX &> "$local_dir/deploy_log.txt"
-  # mamba update -y -p $CONDA_PREFIX "python>=3.9" &>> "$local_dir/deploy_log.txt"
-fi
-
-# Install databases - using aquamis script
-bash "$local_dir/scripts/aquamis_setup.sh" --databases &>> "$local_dir/deploy_log.txt"
-
-# Finalize BUSCO install - modified from scirpt to work with hashed env name
-download_file() {
-  # Init
-  download_success=false
-  download_status=false
-  download_hash=''
-  
-  # Download
-  if [[ -f $local_archive ]] && [[ $arg_force == false ]]; then
-    echo "The file $local_archive already exists. Skipping download."
-  else
-    echo "Downloading $remote_archive to $local_archive"
-    wget --ca-certificate=$local_dir/resources/seafile-bfr-berlin_certificate.pem --output-document $local_archive $remote_archive && download_status=true
-    [[ "$download_status" = true ]] && [[ -s $local_archive ]] && download_hash=$(openssl dgst -r -sha256 $local_archive) && download_success=true
-  fi
-  
-  # Verify Integrity of Download
-  [[ -n $download_hash ]] && echo "$download_hash" >> $hashfile
-
-  # Unpack Downloads
-  [[ "$download_success" == true ]] && echo "Extracting archive $(basename $local_archive) to $extraction_directory"
-  [[ "$download_success" == true ]] && tar -xzv -f $local_archive -C $extraction_directory
-}
-
-mkdir -p "$local_dir/download"
-
-remote_archive="https://gitlab.bfr.berlin/bfr_bioinformatics/aquamis_databases/-/raw/main/augustus.tar.gz" # 139MB or https://databay.bfrlab.de/f/fa13c9eb2625477eb729/?dl=1
-local_archive="$local_dir/download/augustus.tar.gz"
-hashfile="$local_dir/download/reference_db.sha256"
-extraction_directory="$CONDA_PREFIX/lib/python3.7/site-packages/quast_libs/"
-download_file  &>> "$local_dir/deploy_log.txt"
-
-remote_archive="https://gitlab.bfr.berlin/bfr_bioinformatics/aquamis_databases/-/raw/main/bacteria.tar.gz" # 8.8MB or https://busco.ezlab.org/v2/datasets/bacteria_odb9.tar.gz
-local_archive="$local_dir/download/bacteria.tar.gz"
-hashfile="$local_dir/download/reference_db.sha256"
-extraction_directory="$CONDA_PREFIX/lib/python3.7/site-packages/quast_libs/busco/"
-download_file  &>> "$local_dir/deploy_log.txt"
-
-cat "$local_dir/download/reference_db.sha256" >> "$local_dir/reference_db/reference_db.sha256"
-rm -r "$local_dir/download"
+# Finalize instalation with script
+bash $CONDA_PREFIX/opt/aquamis/scripts/aquamis_setup.sh \
+  --env_name $(basename $CONDA_PREFIX) \
+  --busco --databases --auto
